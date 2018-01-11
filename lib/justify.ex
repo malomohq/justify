67af24e423668ed1b67af82eb936623e9b6e0dca
Anthony Smith <anthony@sticksnleaves.com>
2018-01-11 09:09:46 -0500

    First!

---
+++ lib/justify.ex
@@ -0,0 +1,18 @@
+defmodule Justify do
+  @moduledoc """
+  Documentation for Justify.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Justify.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/justify_test.exs
@@ -0,0 +1,8 @@
+defmodule JustifyTest do
+  use ExUnit.Case
+  doctest Justify
+
+  test "greets the world" do
+    assert Justify.hello() == :world
+  end
+end
