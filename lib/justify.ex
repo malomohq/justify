db724272774f611bace2d8867ba0c96fbae5f254
Anthony Smith <anthony@sticksnleaves.com>
2018-01-17 11:16:06 -0500

    remove generated template code

--- lib/justify.ex
+++ lib/justify.ex
@@ -1,18 +1,3 @@
 defmodule Justify do
-  @moduledoc """
-  Documentation for Justify.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Justify.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  
 end
