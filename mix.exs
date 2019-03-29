8dc19c2817e8164ed3b9ec202d14e56c80e59bfa
Anthony Smith <420061+anthonator@users.noreply.github.com>
2019-03-29 10:56:20 -0400

    Added missing comma

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Justify.MixProject do
   def project do
     [
       app: :justify,
-      description: "Data validation for Elixir"
+      description: "Data validation for Elixir",
       version: "1.0.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
