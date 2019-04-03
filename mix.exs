4a521fab65e2faafea6b905b024c79b4b5407f1a
Anthony Smith <420061+anthonator@users.noreply.github.com>
2019-04-03 13:06:31 -0400

    Bump version to 1.0.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Justify.MixProject do
     [
       app: :justify,
       description: "Data validation for Elixir",
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.3",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
