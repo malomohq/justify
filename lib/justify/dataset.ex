6644d48795d6d6dc1d954c4163b46d88f6c4e2f0
Anthony Smith <420061+anthonator@users.noreply.github.com>
2021-12-04 18:48:42 -0500

    Support keyword list and map in dataset

--- lib/justify/dataset.ex
+++ lib/justify/dataset.ex
@@ -4,12 +4,47 @@ defmodule Justify.Dataset do
   @type error_t :: { String.t(), Keyword.t() }
 
   @type t :: %__MODULE__{
-               data: map,
+               data: keyword | map,
                errors: [{ atom, error_t }],
                valid?: boolean
              }
 
-  @spec new(map) :: t
-  def new(%Justify.Dataset{} = dataset), do: dataset
-  def new(data), do: %__MODULE__{ data: data }
+  @doc """
+  Gets the value for a field.
+  """
+  @spec get_field(t, any, any) :: any
+  def get_field(dataset, field, default \\ nil) do
+    data = dataset.data
+
+    cond do
+      Keyword.keyword?(data) ->
+        Keyword.get(data, field, default)
+      is_map(data) ->
+        Map.get(data, field, default)
+    end
+  end
+
+  @doc """
+  Returns a new dataset.
+  """
+  @spec new :: t
+  def new, do: %__MODULE__{}
+
+  @doc """
+  Returns a dataset from a map, keyword list or dataset.
+
+  If a dataset is provided it will be returned as-is.
+  """
+  @spec new(keyword | map | t) :: t
+  def new(%Justify.Dataset{} = dataset) do
+    dataset
+  end
+
+  def new(data) do
+    if Keyword.keyword?(data) || is_map(data) do
+      %__MODULE__{ data: data }
+    else
+      raise ArgumentError, "Justify.Dataset.new/1 expects a keyword list or a map, got: #{inspect(data)}"
+    end
+  end
 end
---
+++ test/justify/dataset_test.exs
@@ -0,0 +1,50 @@
+defmodule Justify.DatasetTest do
+  use ExUnit.Case, async: true
+
+  describe "get_field/3" do
+    test "retrieves value from a map" do
+      field = :field
+
+      value = "value"
+
+      data = Map.new([{ field, value }])
+
+      dataset = Justify.Dataset.new(data)
+
+      assert Justify.Dataset.get_field(dataset, field) == value
+    end
+
+    test "retrieves value from a keyword list" do
+      field = :field
+
+      value = "value"
+
+      data = Keyword.new([{ field, value }])
+
+      dataset = Justify.Dataset.new(data)
+
+      assert Justify.Dataset.get_field(dataset, field) == value
+    end
+
+    test "returns a default value if provided and the field is not present" do
+      default = "default"
+
+      dataset = Justify.Dataset.new()
+
+      assert Justify.Dataset.get_field(dataset, :field, default) == default
+    end
+  end
+
+  test "new/0" do
+    assert Justify.Dataset.new() == %Justify.Dataset{}
+  end
+
+  test "new/1" do
+    keyword = [field: "value"]
+    map = %{ field: "value" }
+
+    assert %Justify.Dataset{ data: ^keyword } = Justify.Dataset.new(keyword)
+    assert %Justify.Dataset{ data: ^map } = Justify.Dataset.new(map)
+    assert_raise ArgumentError, fn -> Justify.Dataset.new("😦") end
+  end
+end
