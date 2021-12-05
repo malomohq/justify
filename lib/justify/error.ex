d16836939814ce02b474d475b4303bde755d6ca9
Anthony Smith <420061+anthonator@users.noreply.github.com>
2021-12-05 03:14:46 -0500

    Added function for raising an exception on validation errors

--- lib/justify/error.ex
+++ lib/justify/error.ex
@@ -5,6 +5,9 @@ defmodule Justify.Error do
 
   defstruct [:field, :message, :opts]
 
+  @type raise_opts_t ::
+          [raise: (t -> no_return)]
+
   @type t ::
           %__MODULE__{
             field: any,
@@ -21,5 +24,40 @@ defmodule Justify.Error do
     |> Map.put(:field, field)
     |> Map.put(:message, message)
     |> Map.put(:opts, opts)
+  end
+
+  @doc """
+  Raises a `Justify.ValidationError` exception based on the provided error.
+
+  To raise an exception other than `Justify.ValidationError` you can provide
+  a function to the `:raise` option that will become responsible for raising the
+  exception.
+
+  ## Example raising a `Justify.ValidationError` exception
+
+      error = Justify.Error.new(:email, "must be valid")
+
+      Justify.Error.raise!(error)
+
+  ## Example raising a custom exception
+
+      error = Justify.Error.new(:email, "must be valid")
+
+      Justify.Error.raise!(error, raise: fn
+        (error) ->
+          raise ArgumentError, "expected \#{inspect(error.field)} to be a valid email address"
+      end)
+  """
+  @spec raise!(t, raise_opts_t) :: no_return
+  defmacro raise!(error, opts \\ []) do
+    quote do
+      raise_fn = Keyword.get(unquote(opts), :raise)
+
+      if raise_fn do
+        raise_fn.(unquote(error))
+      else
+        raise Justify.ValidationError, error: unquote(error)
+      end
+    end
   end
 end
--- test/justify/error_test.exs
+++ test/justify/error_test.exs
@@ -3,6 +3,8 @@ defmodule Justify.ErrorTest do
 
   alias Justify.{ Error }
 
+  require Justify.Error
+
   test "new/3" do
     field = :field
 
@@ -15,5 +17,36 @@ defmodule Justify.ErrorTest do
              message: ^message,
              opts: ^opts
            } = Justify.Error.new(field, message, opts)
+  end
+
+  describe "raise/2" do
+    test "raises a Justify.ValidationError by default" do
+      field = :field
+
+      message = "message"
+
+      error = Justify.Error.new(field, message)
+
+      assert_raise Justify.ValidationError, fn ->
+        Justify.Error.raise!(error)
+      end
+    end
+
+    test "raise a custom exception when provided" do
+      field = :field
+
+      message = "message"
+
+      error = Justify.Error.new(field, message)
+
+      raise_fn = fn
+        (^error) ->
+          raise "my custom error"
+      end
+
+      assert_raise RuntimeError, "my custom error", fn ->
+        Justify.Error.raise!(error, raise: raise_fn)
+      end
+    end
   end
 end
