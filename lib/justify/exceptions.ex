27f4cc18c9a07cacd2081767b57db4593c1338d1
Anthony Smith <420061+anthonator@users.noreply.github.com>
2021-12-05 00:10:46 -0500

    Created exception Justify.ValdiationError

--- lib/justify/exceptions.ex
+++ lib/justify/exceptions.ex
@@ -1,8 +1,27 @@
-defmodule Justify.NoDatasetError do
+defmodule Justify.BadStructError do
   @moduledoc """
-  Raised when a function is expected to return a `Justify.Dataset` struct but
-  does not.
+  Raised at runtime when a function is expected to return a `Justify.Dataset`
+  struct but does not.
   """
-  
+
   defexception [:message]
+end
+
+defmodule Justify.ValidationError do
+  @moduledoc """
+  Raised at runtime when a validation error occurs.
+  """
+
+  defexception [:message]
+
+  def message(field, message) do
+    "field #{inspect(field)} failed validation with message " <>
+    "#{inspect(message)}"
+  end
+
+  def exception(opts) do
+    error = Keyword.fetch!(opts, :error)
+
+    %__MODULE__{ message: message(error.field, error.message) }
+  end
 end
